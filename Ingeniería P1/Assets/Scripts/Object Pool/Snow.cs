6ed565a2ea28f6363f03f0f6212676696987499c
Santiago Rubio Montero <srubiomontero@gmail.com>
2024-01-07 14:37:38 +0100

    poler

--- Ingeniería P1/Assets/Scripts/Object Pool/Snow.cs
+++ Ingeniería P1/Assets/Scripts/Object Pool/Snow.cs
@@ -7,9 +7,10 @@ public class Snow : MonoBehaviour
     public float snowSpeed = 1f;
     public void Start()
     {
-        float yForce = Random.Range(snowSpeed / 2f, snowSpeed);
-
-        Vector3 force= new Vector3(0,yForce, 0);
+        float xForce = Random.Range(snowSpeed / 10f, 0);
+        float yForce = Random.Range(snowSpeed / 10f, 10);
+        float zForce = Random.Range(snowSpeed / 10f, 0);
+        Vector3 force= new Vector3(xForce , yForce, zForce);
 
         GetComponent<Rigidbody>().velocity = force;
     }
