1b3872d6fa6aa061375f1a29c5a4f187d2112222
Santiago Rubio Montero <srubiomontero@gmail.com>
2024-01-13 21:16:51 +0100

    wincon

--- Ingeniería P1/Assets/Scripts/Patterns/Object Pool/Snow.cs
+++ Ingeniería P1/Assets/Scripts/Patterns/Object Pool/Snow.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Snow : MonoBehaviour
 {
-    public float upForce = 0.5f;
-    //public float sideForce = .1f;
+    public float upForce = 1f;
+    public float sideForce = .1f;
     public void Start()
     {
-        float xForce = Random.Range(-upForce, upForce);
+        float xForce = Random.Range(-sideForce, sideForce);
         float yForce = Random.Range(upForce / 2f, upForce);
-        float zForce = Random.Range(-upForce, upForce);
+        float zForce = Random.Range(-sideForce, sideForce);
         Vector3 force= new Vector3(xForce , yForce, zForce);
 
         GetComponent<Rigidbody>().velocity = force;
